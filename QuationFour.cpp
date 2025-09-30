446ec412cee03da4f261142e338db2b1bb042002
Roman Bondarenko <rbondarenko@globalgiving.org>
2025-09-30 21:47:49 +0300

    Add .gitignore to ignore build files

--- QuationFour.cpp
+++ QuationFour.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main() {
-    double n, sum;
-    cout << "Enter a number: ";
+    double x, sum, pi = 3.14;
+    int n;
+    cout << "Введіть x: ";
+    cin >> x;
+    cout << "Введіть n: ";
     cin >> n;
-    for (int i = 1; i <= n; i++) {}
+    for (int k = 1; k <= n; k++) {
+        sum += pow(x, k) * sin(k * pi / 3) / k;
+    }
+    cout << "Сума " << n << " членів ряду = " << sum << endl;
 }
